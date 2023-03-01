bced5777255efee947e1b258f87844afdd360873
modpilot213 <125748822+modpilot213@users.noreply.github.com>
2023-03-01 10:29:55 -0500

    Update streamlit_app.py
    added breakfast menu to match snowflake course

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit 
+
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('breakfast menu')
-streamlit.text('streamed chicken')
-streamlit.text('spinach')
-streamlit.text('thats it just enjoy it')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
